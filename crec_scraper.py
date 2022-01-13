f8af16b17c6096fd2714c901f6126f4e7da39664
Bradley Emmons <43657042+ActivePhnx@users.noreply.github.com>
2022-01-12 19:50:52 -0500

    Update Python Scraper

--- crec_scraper.py
+++ crec_scraper.py
@@ -4,16 +4,19 @@ import datetime as dt
 import io
 import lxml.html as html
 import sqlite3
+import time
 
 
-D = dt.date.today() - dt.timedelta(days=2)
+#D = dt.date.today() - dt.timedelta(days=2)
 
 
-def get_html_text():
-    r = requests.get(
-        "http://www.gpo.gov/fdsys/pkg/CREC-{0}.zip"
-        .format(D.strftime("%Y-%m-%d")))
-    if r.status_code == 404:
+def get_html_text(date):
+    print(date)
+    thing = "https://www.govinfo.gov/link/crec/latest?link-type=zip&publishdate=" + date
+    print(thing.format(date))
+    r = requests.get(thing.format(date))
+    print(r.status_code)
+    if r.status_code == 404 or r.status_code == 400 or r.status_code == 503:
         r.raise_for_status()
     else:
         zip_content = io.BytesIO(r.content)
@@ -24,10 +27,10 @@ def get_html_text():
                 zip_read.append(zipfile.ZipFile(zip_content).read(str(i)))
         for i in range(len(zip_read)):
             html_data = html.fromstring(zip_read[i]).text_content()
-            add_to_db(html_data)
+            add_to_db(html_data, date)
 
 
-def add_to_db(html_data):
+def add_to_db(html_data, date):
     conn = sqlite3.connect('crec.db')
     cur = conn.cursor()
 
@@ -37,13 +40,28 @@ def add_to_db(html_data):
 
     cur.execute('''
               INSERT or IGNORE INTO crec VALUES
-              (?, ?, ?)''', (None, html_data, D))
+              (?, ?, ?)''', (None, html_data, date))
 
     conn.commit()
     conn.close()
 
 
-try:
-    get_html_text()
-except requests.HTTPError:
-    pass
+
+for y in range(5):
+    for m in range(11):
+        for d in range(31):
+            try:
+                year = str(2016 + y)
+                month = str(1 + m)
+                day = str(1 + d)
+                if m < 9:
+                    month = "0" + month
+                if d < 9:
+                    day = "0" + day
+
+                print("trying day" + day)
+                get_html_text(year + "-" + month + "-" + day)
+                time.sleep(.05)
+            except requests.HTTPError:
+               pass
+
