69f0bf91d0e6885c706f29d9e50946e7a2f91a95
Bradley Emmons <43657042+ActivePhnx@users.noreply.github.com>
2022-01-19 12:06:29 -0500

    Updated Scraper (Error db)

--- crec_scraper.py
+++ crec_scraper.py
@@ -16,7 +16,11 @@ def get_html_text(date):
     print(thing.format(date))
     r = requests.get(thing.format(date))
     print(r.status_code)
-    if r.status_code == 404 or r.status_code == 400 or r.status_code == 503:
+    if r.status_code == 404 or r.status_code == 400:
+        r.raise_for_status()
+    elif r.status_code == 503 or r.status_code == 502:
+        print("Adding to error")
+        add_to_error(date)
         r.raise_for_status()
     else:
         zip_content = io.BytesIO(r.content)
@@ -42,13 +46,29 @@ def add_to_db(html_data, date):
               INSERT or IGNORE INTO crec VALUES
               (?, ?, ?)''', (None, html_data, date))
 
+
+    conn.commit()
+    conn.close()
+
+def add_to_error(date):
+    conn = sqlite3.connect('error.db')
+    cur = conn.cursor()
+
+    cur.execute('''CREATE TABLE if not exists error
+                (id INTEGER PRIMARY KEY,
+                 UTC DATE)''')
+
+    cur.execute('''
+              INSERT or IGNORE INTO error VALUES
+              (?, ?)''', (None, date))
+    print("Added " + date)
     conn.commit()
     conn.close()
 
 
 
-for y in range(5):
-    for m in range(11):
+for y in range(6):
+    for m in range(12):
         for d in range(31):
             try:
                 year = str(2016 + y)
@@ -59,9 +79,25 @@ for y in range(5):
                 if d < 9:
                     day = "0" + day
 
-                print("trying day" + day)
                 get_html_text(year + "-" + month + "-" + day)
-                time.sleep(.05)
+                time.sleep(.1)
             except requests.HTTPError:
                pass
 
+
+
+""" Testing Error db
+for y in range(6):
+    for m in range(12):
+        for d in range(31):
+            year = str(2016 + y)
+            month = str(1 + m)
+            day = str(1 + d)
+            if m < 9:
+                month = "0" + month
+            if d < 9:
+                day = "0" + day
+                
+            add_to_503(year + "-" + month + "-" + day)
+"""
+                
